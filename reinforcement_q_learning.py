0c0944f3f190c29a651b625cad25ab610c360284
ingmarliibert <ingmar.liibert@gmail.com>
2021-01-14 23:38:56 +0000

    stufs

--- reinforcement_q_learning.py
+++ reinforcement_q_learning.py
@@ -58,6 +58,7 @@ We'll also use the following from PyTorch:
 """
 
 import gym
+from gym import wrappers
 import math
 import random
 import numpy as np
@@ -66,15 +67,20 @@ import matplotlib.pyplot as plt
 from collections import namedtuple
 from itertools import count
 from PIL import Image
+from itertools import count
+import warnings
+import json
+
+warnings.filterwarnings("ignore", category=UserWarning)
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
+import time
 
-
-env = gym.make('CartPole-v0').unwrapped
+env = gym.make('CartPole-v1').unwrapped
 
 # set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
@@ -282,12 +288,6 @@ def get_screen():
 
 
 env.reset()
-plt.figure()
-plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
-           interpolation='none')
-plt.title('Example extracted screen')
-plt.show()
-
 
 ######################################################################
 # Training
@@ -317,6 +317,9 @@ EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 200
 TARGET_UPDATE = 10
+
+ONE_HOUR = 60 * 60
+TIME_LIMIT = 2 * ONE_HOUR
 
 # Get screen size so that we can initialize layers correctly based on shape
 # returned from AI gym. Typical dimensions at this point are close to 3x40x90
@@ -356,16 +359,17 @@ def select_action(state):
 
 
 episode_durations = []
+episode_scores = []
 
 
 def plot_durations():
     plt.figure(2)
     plt.clf()
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
+    durations_t = torch.tensor(episode_scores, dtype=torch.float)
+    plt.title('DQN training results')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Score')
+    plt.plot(episode_durations, durations_t.numpy())
     # Take 100 episode averages and plot them too
     if len(durations_t) >= 100:
         means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
@@ -440,6 +444,8 @@ def optimize_model():
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
+def save_agent(agent, filename):
+    torch.save(agent.state_dict(), filename)
 
 ######################################################################
 #
@@ -453,49 +459,76 @@ def optimize_model():
 # the notebook and run lot more epsiodes, such as 300+ for meaningful
 # duration improvements.
 #
-
-num_episodes = 50
-for i_episode in range(num_episodes):
-    # Initialize the environment and state
-    env.reset()
-    last_screen = get_screen()
-    current_screen = get_screen()
-    state = current_screen - last_screen
-    for t in count():
-        # Select and perform an action
-        action = select_action(state)
-        _, reward, done, _ = env.step(action.item())
-        reward = torch.tensor([reward], device=device)
-
-        # Observe new state
-        last_screen = current_screen
-        current_screen = get_screen()
-        if not done:
-            next_state = current_screen - last_screen
-        else:
-            next_state = None
-
-        # Store the transition in memory
-        memory.push(state, action, next_state, reward)
-
-        # Move to the next state
-        state = next_state
-
-        # Perform one step of the optimization (on the target network)
-        optimize_model()
-        if done:
-            episode_durations.append(t + 1)
-            plot_durations()
+if True:
+    start_time = time.time()
+    best = None
+    best_score = 0
+    for i_episode in count():
+        # Initialize the environment and state
+        if time.time() - start_time >= TIME_LIMIT:
+            save_agent(best[0], "dqn_best_policy.pth")
+            save_agent(best[1], "dqn_best_target.pth")
             break
-    # Update the target network, copying all weights and biases in DQN
-    if i_episode % TARGET_UPDATE == 0:
-        target_net.load_state_dict(policy_net.state_dict())
+        env.reset()
+        last_screen = get_screen()
+        current_screen = get_screen()
+        state = current_screen - last_screen
+        sum_ = 0
+        print("Episode", i_episode)
+        for t in count():
+            # Select and perform an action
+            action = select_action(state)
+            _, reward, done, _ = env.step(action.item())
+            sum_ += reward
+            reward = torch.tensor([reward], device=device)
 
-print('Complete')
-env.render()
-env.close()
-plt.ioff()
-plt.show()
+            # Observe new state
+            last_screen = current_screen
+            current_screen = get_screen()
+            
+            if not done:
+                next_state = current_screen - last_screen
+            else:
+                next_state = None
+
+            # Store the transition in memory
+            memory.push(state, action, next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the target network)
+            optimize_model()
+            if done:
+                episode_scores.append(sum_)
+                episode_durations.append(time.time() - start_time)
+                if sum_ > best_score:
+                    best = (policy_net, target_net)
+                    best_score = sum_
+                plot_durations()
+                break
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+
+    with open("dqn_data.txt", "w") as file:
+        file.write(",".join(map(str, episode_scores)))
+        file.write("\n")
+        file.write(",".join(map(str, episode_durations)))
+
+    print('Complete')
+    env.render()
+    env.close()
+    plt.ioff()
+    plt.savefig("dqn_plot.png")
+
+def load_agent(filename):
+    agent = DQN(screen_height, screen_width, n_actions)
+        
+    agent.load_state_dict(torch.load(filename))
+    
+    return agent
 
 ######################################################################
 # Here is the diagram that illustrates the overall resulting data flow.
